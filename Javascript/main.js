402f482191aec50078e3100f91bfc5eee810e839
Daoud-Hussain <daoudhussain302@gmail.com>
2024-03-11 19:26:16 +0500

    Frequently Asked questions

--- Javascript/main.js
+++ Javascript/main.js
@@ -2,3 +2,23 @@ function toggleMenu() {
     var navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('show');
 }
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle => {
+	toggle.addEventListener('click', () => {
+		toggle.parentNode.classList.toggle('active');
+	});
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
