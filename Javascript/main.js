ee6f9f2cd1ee7bc4ed23f18648ce2bb709948068
Daoud-Hussain <daoudhussain302@gmail.com>
2024-03-12 15:29:41 +0500

    Creating FAQ

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,15 +10,3 @@ toggles.forEach(toggle => {
 	});
 });
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
